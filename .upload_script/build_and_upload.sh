e32b72097dd0dc0fd88114a0e3cfd863a7e3f717
DavidAce <aceituno@kth.se>
2019-12-16 16:30:21 +0100

    removed upload script

--- .upload_script/build_and_upload.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conan create . davidace/stable --profile=dmrg --build=missing
-conan upload ceres-solver/2.0.0@davidace/stable  -r conan-dmrg --all
-
-#conan create . conan-dmrg/stable --profile=dmrg --build=missing
-#conan upload ceres-solver/1.14.0@conan-dmrg/stable  -r conan-dmrg --all
